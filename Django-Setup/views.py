9f92333a4017e2e8f7e125e90f342e1cab8a4dba
Sal <68786450+saljr324@users.noreply.github.com>
2023-04-06 10:50:05 -0700

    Update views.py

--- Django-Setup/views.py
+++ Django-Setup/views.py
@@ -8,20 +8,32 @@ import subprocess
 # request handler 
 # action 
 
-
 def search_engine(request):
     if request.method == 'POST':
         # If the form was submitted, process the form data
         query = request.POST.get('query')
         # Use the form data to generate some output
-        print("working")
         script_output = subprocess.check_output(['python', 'playground/Search_Repo_Test.py', query])
-        print('script done')
-        decoded = script_output.decode('utf-8')
-        print('done')
-        return render(request, 'search_output.html', {'result': decoded})
+        decoded = script_output.decode('utf-8')[1:-2]
+        repo_list = ast.literal_eval(decoded)
+        repo_list = list(repo_list)
+        print(repo_list)
+
+        url = 'https://raw.githubusercontent.com/ds-modules/Search-Engine/main/tags/tags.json'
+        resp = requests.get(url)
+        tags = json.loads(resp.text)
+        print(tags['BUDS-su20']['department'])
+        departments, names, semesters = [], [], []
+        for i in repo_list:
+            departments.append(tags[i]['department'])
+            names.append(tags[i]['name'])
+            semesters.append(tags[i]['semester'])
+
+        zipped_list = zip(repo_list,departments, names, semesters)
+
+        return render(request, 'index-testing.html', {"information":zipped_list})
         # If the form was not submitted, render the form template
         redirect(reverse('search_engine'))
-    #     return redirect('output_page', query)
+        # return redirect('output_page', query)
     else:
-        return render(request, 'form.html')
+        return render(request, 'index-testing.html')
