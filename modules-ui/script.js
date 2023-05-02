2cc5acd2c67cde1ac3c32f7482bfb714ef4b12dd
Vishali Vallioor <vishalivallioor@Vishalis-MacBook-Pro.local>
2023-05-01 17:30:31 -0700

    Buttons working

--- modules-ui/script.js
+++ modules-ui/script.js
@@ -15,3 +15,22 @@ observer.observe(slideInText2);
 setTimeout(function() {
   document.querySelector(".i1").classList.add("zoom-in"); /* add the class to start the animation */
 }, 3000);
+
+
+function scrollToHalf() {
+  // Calculate the halfway point of the page
+  const halfwayPoint = document.documentElement.scrollHeight / 4.5;
+
+  // Scroll to the halfway point of the page
+  document.documentElement.scrollTop = halfwayPoint;
+  document.body.scrollTop = halfwayPoint;
+}
+
+function scrollToBottom() {
+  // Calculate the halfway point of the page
+  const bottomPoint = document.documentElement.scrollHeight / 1.8;
+
+  // Scroll to the halfway point of the page
+  document.documentElement.scrollTop = bottomPoint;
+  document.body.scrollTop = bottomPoint;
+}
