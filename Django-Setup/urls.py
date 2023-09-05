5fbfc1746a39493d33f432895959ac30251e0ed4
Sal <68786450+saljr324@users.noreply.github.com>
2023-09-04 22:02:36 -0700

    Delete Django-Setup directory

--- Django-Setup/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views 
-
-# URLConf 
-
-urlpatterns = [
-    path('search/', views.search_engine, name = 'search_engine'),
-    path('output_page/', views.output_page, name = 'output_page')
-]
